e8c8083bbd7e0ac2f8373cb94653eee86755d5fc
ajaykumar30071996 <ajayharish034@gmail.com>
2024-01-22 10:41:19 +0200

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
 console.log("Ds-Algo");
+
+
+const array = new Array(100).fill("Ajjay");
+
+function findAjay(arr: string[]) {
+    let t0 = performance.now();
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] === 'Ajjay') {
+            console.log("found");
+        }
+    }
+    let t1 = performance.now();
+    console.log("Time taken for the program is ", t1 - t0, " MS");
+}
+
+findAjay(array)
